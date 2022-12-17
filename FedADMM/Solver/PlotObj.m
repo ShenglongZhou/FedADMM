33e32027b40738d38dd1d1e67e3c51aadcc2f87d
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-17 11:31:47 +0000

    Update PlotObj.m

--- FedADMM/Solver/PlotObj.m
+++ FedADMM/Solver/PlotObj.m
@@ -1,12 +1,11 @@
-function  PlotObj(obj1)
+function  PlotObj(obj,k0)
     figure('Renderer', 'painters', 'Position',[1100 400 370 320]);
     axes('Position', [0.14 0.14 0.83 0.8] ); 
-    h1 = plot(1:length(obj1),obj1(1:end)); hold on
-    %h2 = plot(1:length(obj2),obj2(1:end)); hold on
+    y  = obj(1:k0:end);
+    h1 = plot(1:length(y),y); hold on
     grid on
-    h1.LineWidth  = 1.5;   %h2.LineWidth  = 1.5;       
-    h1.LineStyle  = '-';   %h2.LineStyle  = '--'; 
-    h1.Color = '#3caea3';  %h2.Color = '#ed553b';
-    %legend('FedGiA_G','FedGiA_D','location','NorthEast')
+    h1.LineWidth  = 1.5;      
+    h1.LineStyle  = '-';   
+    h1.Color = '#3caea3';  
     xlabel('Iterations'); ylabel('Objective');    
 end
