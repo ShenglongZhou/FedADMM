448bfdf617e6835fe5fadd3eaac8f43d2680b38e
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-17 11:16:15 +0000

    Update demon.m

--- FedADMM/demon.m
+++ FedADMM/demon.m
@@ -1,21 +1,23 @@
 clc; clear; close all;
 addpath(genpath(pwd));
 
- 
 Prob = {'LinReg','LogReg'};
 prob  = Prob{1};
 
 m     = 100;
 switch prob 
     case 'LinReg' 
-        n     = 100;
-        var1  = 1/3;
-        var2  = 1/3;
+        n       = 100;
+        var1    = 1/3;
+        var2    = 1/3;
+        pars.r0 = 0.5; %Increase this value if you find the solver diverges
     case 'LogReg'
-        var1  = load('toxicity.mat').X; 
-        var2  = load('toxicityclass.mat').y; 
-        n     = size(var1,2);
+        var1    = load('toxicity.mat').X; 
+        var2    = load('toxicityclass.mat').y; 
+        n       = size(var1,2); 
+        pars.r0 = 0.05; %Increase this value if you find the solver diverges
+
 end 
 
 [A,b,dim,n] = DataGeneration(prob,m,n,var1,var2); 
-out         = FedADMM(dim,n,A,b,10,prob);
+out         = FedADMM(dim,n,A,b,10,prob,pars);
