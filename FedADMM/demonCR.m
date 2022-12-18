a6a1429a05dd491ffb4ddb5469833d1001a953ab
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-18 18:02:03 +0000

    Update demonCR.m

--- FedADMM/demonCR.m
+++ FedADMM/demonCR.m
@@ -20,23 +20,20 @@ switch prob
 end 
 [A,b,dim,n] = DataGeneration(prob,m,n,var1,var2); 
  
-k0         = [1 5 10 15 20];
+k0         = [10:10:50];
 out        = cell(1,nnz(k0)); 
 for i      = 1:nnz(k0)
     out{i} = FedADMM(dim,n,A,b,k0(i),prob,pars);  
 end
 
 figure('Renderer', 'painters', 'Position',[800 200 504 460]);
-axes('Position', [0.115 0.11 0.86 0.85] ); 
-styles  = {'--','-.','-','-.','--'}; 
+axes('Position', [0.115 0.11 0.86 0.85] );  
 colors = {'#173f5f','#20639b','#3caea3','#f6d55c','#ed553b'};
 for i = 1 : nnz(k0)
-    mi = length(1:k0(i):out{i}.iter);
-    pl = semilogx(1:mi,out{i}.OBJ(1:k0(i):end),styles{i}); hold on
+    mi = length(1:out{i}.iter);
+    pl = semilogx(1:mi,out{i}.OBJ(1:end),'-'); hold on
     pl.Color     = colors{i};
     pl.LineWidth = 2; 
     leg{i} = strcat('k_0=', num2str(k0(i)));
 end
-ylabel('Objective'); xlabel('CR'); legend(leg); grid on
-
-
+ylabel('Objective'); xlabel('Iteration'); legend(leg); grid on
